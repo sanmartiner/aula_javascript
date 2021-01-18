14d635eaf1aaa0d5a13cdfa4e550afed4d714847
David San Martín <66731842+sanmartiner@users.noreply.github.com>
2021-01-18 10:29:53 -0400

    Tipagem dinamica, string e valores inteiros

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var lista = ["maca", "pera", "laranja"];
+console.log(lista[1]);
+console.log(lista[0]);
+lista.push("uva");
+console.log(lista);
+/*var nome = "David San Martín";
+var idade = 27;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+var n1 = 5;
+var n2 = 3;
+//alert(nome +" tem "+idade+" anos ");
+alert(idade+idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"))
+console.log(frase.toUpperCase);
+console.log(frase.toLowerCase);
+//alert(frase.replace("Japão", "Brasil"));
+console.log(n1 * n2);*/
