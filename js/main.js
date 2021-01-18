ab330caf7f0ef1a43bd26a8f0bd06abe15492287
David San Martín <66731842+sanmartiner@users.noreply.github.com>
2021-01-18 10:46:26 -0400

    Arrays e dicionarios

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-var lista = ["maca", "pera", "laranja"];
+var frutas =[{nome:"maca", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*var fruta = {nome:"Maca", cor: "vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+/*var lista = ["maca", "pera", "laranja"];
 console.log(lista[1]);
 console.log(lista[0]);
 lista.push("uva");
+//lista.pop();
 console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+
+console.log(lista.toString()[0]);
+
+console.log(lista.join(" | "));*/
+
 /*var nome = "David San Martín";
 var idade = 27;
 var idade2 = 10;
