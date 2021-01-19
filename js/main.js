12df196260b8ad3a61976e09fdbc589ad78548b0
David San Martín <66731842+sanmartiner@users.noreply.github.com>
2021-01-18 22:10:28 -0400

    Loops e Date

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-var idade = prompt("Qual a sua idade");
+var d = new Date();
+//alert(d.getMonth()+1);
+//alert(d.getMinutes());
+alert(d.getDay());
+
+/*var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}*/
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    count++;
+}*/
+
+/*var idade = prompt("Qual a sua idade");
 if(idade >= 18){
-    alert("maior de idade")
+    alert("maior de idade");
 } else {
-    alert("menor de idade")
-}
+    alert("menor de idade");
+}*/
 
 
 /*var frutas =[{nome:"maca", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
