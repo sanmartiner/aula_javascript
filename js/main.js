51fa6c2ebb1c7ab2dc869d95bb6396291caa152a
David San Martín <66731842+sanmartiner@users.noreply.github.com>
2021-01-19 10:36:24 -0400

    onmouseover onmouseout onchang onload

--- js/main.js
+++ js/main.js
@@ -5,9 +5,28 @@ function clique(){
 }
 
 function redirecionar(){
-    window.open("https://facebook.com/");
+    //window.open("https://facebook.com/");
+    window.location.href = "https://facebook.com";
 }
 
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
 /*function soma(n1, n2){
     return(n1 + n2)
 }
